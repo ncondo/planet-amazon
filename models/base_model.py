1263107bd56a2d3ef494c4c9fb49d605e0b699cd
Nicholas Condo <nicholas.condo@gmail.com>
2017-07-15 01:04:55 -0400

    refactor: Create models in separate module for quicker training and testing

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+import keras as k
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import SGD
+
+
+def base_model(img_rows, img_cols, num_channels):
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(img_rows,img_cols,num_channels)))
+    model.add(Conv2D(32, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(Conv2D(32, (3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(256, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3,3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(17, activation='sigmoid'))
+
+    #sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    #model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
